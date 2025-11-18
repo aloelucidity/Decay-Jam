e2c056b89e8aa9ea9097ba45bd52badff906c126
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2025-11-18 11:20:10 +0100

    ability buttons that i spent way too much time on

--- level/ui/animate_gradient.gd
+++ level/ui/animate_gradient.gd
@@ -1,11 +1,14 @@
-extends TextureRect
+class_name AnimateGradient
+extends Control
 
 
 @export var animate_index: int = 1
 @export var animate_range := Vector2(0.4, 0.6)
 @export var animate_speed: float = 1
 
+var variable_id: String = "texture"
+
 func _process(_delta: float) -> void:
 	var elapsed: float = Time.get_unix_time_from_system()
 	var difference: float = animate_range.y - animate_range.x
-	texture.gradient.offsets[animate_index] = animate_range.x + difference/2 + (difference * sin(elapsed * animate_speed))
+	self[variable_id].gradient.offsets[animate_index] = animate_range.x + difference/2 + (difference * sin(elapsed * animate_speed))
